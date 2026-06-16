7d938136f0ce6ee1a8500cf65317855bc802c91e
rodyherrera <contact@rodyherrera.com>
2026-06-16 02:08:52 -0400

    fix: update version to 2.0.2 in conanfile.py, plugin.json, and vpm.json; adjust dependencies for coretoolkit and structure-identification

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 class PatternStructureMatchingConan(ConanFile):
     name = "pattern-structure-matching"
-    version = "2.0.1"
+    version = "2.0.2"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
@@ -13,8 +13,8 @@ class PatternStructureMatchingConan(ConanFile):
         "boost/1.88.0",
         "onetbb/2021.12.0",
         "common-neighbor-analysis/[>=2.0]",
-        "coretoolkit/[>=2.0]",
-        "structure-identification/[>=2.0]",
+        "coretoolkit/[>=2.5]",
+        "structure-identification/[>=2.1]",
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
         "yaml-cpp/0.8.0",
